b3aeb9811f144742e81acdaa79eabaaf1f7ac99e
Kyrylo Vlasenko <s24841@pjwstk.edu.pl>
2025-02-05 23:04:09 +0530

    hotfix

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,5 @@
 import './NavBar.css';
 import moon from '../../assets/moon.svg';
-import logo from '../../assets/logo.svg';
 import sun from '../../assets/sun.svg';
 import exit from '../../assets/exit.svg';
 import profile from '../../assets/profile.svg';
@@ -18,10 +17,14 @@ export default function NavBar() {
 
   return (
     <nav className="outer">
+      <link
+        href="https://fonts.googleapis.com/css?family=Akronim"
+        rel="stylesheet"
+      ></link>
       <ul>
         <li className="nav-link">
-          <Link to="/" >
-            <img style={{ maxWidth: '20%', margin: '10px 0 10px 20px' }} src={logo} alt="Logo" />
+          <Link to="/" style={{ textDecoration: 'none', color: '#BBD8EB' }}>
+            <p className="logo">HAVOC</p>
           </Link>
         </li>
         <li className="nav-links">
@@ -39,29 +42,7 @@ export default function NavBar() {
   );
 }
 
-function ButtonsNotLoggedIn({ icon, onClickTheme }) {
-  return (
-    <ul className="nav-links-list">
-      <li className="nav-link">
-        <button className="tab-button" onClick={onClickTheme}>
-          <img src={icon} />
-        </button>
-      </li>
-      <li className="nav-link">
-        <Link to="/login">
-          <button className="a-button">Sign In</button>
-        </Link>
-      </li>
-      <li className="nav-link">
-        <Link to="/register">
-          <button className="yes-button">Sign Up</button>
-        </Link>
-      </li>
-    </ul>
-  );
-}
-
-function ButtonsLoggedIn({ icon, onClickTheme, onLogOut }) {
+function ButtonsLoggedIn({ icon, onClickTheme, onLogOut, onProfileClick }) {
   return (
     <ul className="nav-links-list">
       <li className="nav-link">
