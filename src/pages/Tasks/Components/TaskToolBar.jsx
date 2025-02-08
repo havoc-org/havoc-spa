df4ebfc846dc765afb366d7cf2d1fa6469bc30eb
SolarDefender <vladimirkalinin67@gmail.com>
2025-02-08 11:37:38 +0100

    added task sort

--- src/pages/Tasks/Components/TaskToolBar.jsx
+++ src/pages/Tasks/Components/TaskToolBar.jsx
@@ -3,35 +3,62 @@ import { Link } from 'react-router-dom';
 import createButton from '../../../assets/AddNewTask.svg';
 import exitButton from '../../../assets/projectexit.svg';
 import useProject from '../../../hooks/useProject';
+import { left } from '@cloudinary/url-gen/qualifiers/textAlignment';
 
-const TaskToolBar = () => {
+const TaskToolBar = ({ sortValue, setSortValue }) => {
+  const handleSortChange = (event) => {
+    setSortValue(event.target.value);
+  };
   const perm = useProject();
+  
   if (perm.isDeveloper()) {
-    return (<Exit />);
+    return (
+    <div className="task-toolbar"> 
+      <Sort sortValue={sortValue} handleSortChange={handleSortChange} />
+      <Exit />
+    </div>
+  );
   } else if (perm.isManager()) {
     return (
       <div className="task-toolbar">
-        <Exit />
-        <div className="task-toolbar">
-          <Create />
+        <div style={{display: "flex"}}>
+        <Create />
+        <Sort sortValue={sortValue} handleSortChange={handleSortChange}/>
         </div>
+        <Exit />
       </div>
     );
   }
   else if (perm.isOwner()) {
     return (
       <div className="task-toolbar">
-        <Exit />
+        <div style={{display: "flex"}}>
+        <Create />
+        <Sort sortValue={sortValue} handleSortChange={handleSortChange}/>
+        </div>
         <div className="task-toolbar">
         <Link to="edit">
             <button className="edit-project">Edit Project</button>
           </Link>
-          <Create />
+          <Exit />
         </div>
       </div>
     );
   }
 };
+
+function Sort({ sortValue, handleSortChange }) {
+  return (
+    <div className="sort-container" style={{display: "flex"}}>
+      <label>Sort By:</label>
+      <select value={sortValue} onChange={handleSortChange}>
+        <option value="deadline">Deadline</option>
+        <option value="startDate">Start date</option>
+        <option value="name">Name(a-z)</option>
+      </select>
+    </div>
+  );
+}
 
 function Exit() {
     return (
